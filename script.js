43259bed4fa83ca132d07afeddd2625b9813ea9d
Waqas Qasem <71858112+wsarori@users.noreply.github.com>
2024-08-07 21:35:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+response1=await  fetch("travel_recommendation_api.json");
+//.then(response => response.json())
+alert(response1.status);
+
+
+
+      
+      //const searchTerm = document.getElementById('edit_36b3f34d').value.toLowerCase();
+      //let results = []; // Array to store search results
+      //document.getElementById('edit_36b3f34d').value='hallo';
+
+      // Search logic for countries, cities, temples, beaches
+      //if (searchTerm === 'countries' || searchTerm === 'country') {
+        //results = travelData.countries;
+        //document.getElementById('edit_36b3f34d').value=toString(travelData);
+      ///} else if (searchTerm === 'temples' || searchTerm === 'temple') {
+        //results = travelData.temples;
+      //} else if (searchTerm === 'beaches' || searchTerm === 'beach') {
+        //results = travelData.beaches;
+      //} else if (searchTerm === 'cities') { // Add search for cities
+        // Loop through countries and add matching cities
+        //results = travelData.countries.flatMap(country => country.cities);
+      //} else {
+      //  // Handle no match case (optional: display message)
+      //}
+
+      // Update container content (clear previous results)
+      //container_21f0fe30_padding.innerHTML = '';
+
+      // Display search results (modify based on your needs)
+      //if (results.length > 0) {
+        //document.getElementById('edit_36b3f34d').value='hallo';
+
+        //results.forEach(result => {
+         // container_21f0fe30_padding.innerHTML += `
+           // <h3>${result.name}</h3>
+           // <img src="${result.imageUrl}" alt="${result.name}">
+           // <p>${result.description}</p>
+          //;
+       // });
+      //} else {
+      //  container_21f0fe30_padding.innerHTML = 'No results found.'; // Example message
+    //  }
+  //  });
+
